e2b5de6a6310261b9bea6ea64315b0d6d8e6fe26
ivostinghen <gamefico@gmail.com>
2019-02-06 22:25:42 -0400

    Scenario Composition

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+    Transform target;
+    public float speed;
+    Vector3 pos;
+    public Vector3 offSet;
+
+    private void Start()
+    {
+        target = GameObject.Find("Character").transform;
+    }
+
+   
+    void LateUpdate () {
+        pos = target.position;
+        pos += offSet;
+        transform.position = Vector3.Lerp(transform.position,pos, Time.deltaTime * speed);
+	}   
+}
