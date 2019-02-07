b0d834a50ad8847f2c4d3cdcd5a6d4b42f4bc6ab
ivostinghen <gamefico@gmail.com>
2019-02-07 03:12:00 -0400

    Add Besier Curve

---
+++ Assets/Scripts/BesierCurve.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BesierCurve : MonoBehaviour
+{
+    [SerializeField]
+    private float besierHeight = 5;
+    private float besierTime;
+    private bool isRunning;
+    private Vector3 A, B, C;
+    [SerializeField]
+    private float forwardDistance;
+    [SerializeField]
+    private float speed;
+
+    private Vector3 CalculateBesier(float t)
+    {
+        besierTime += Time.deltaTime * speed / 100;
+        Vector3 besierPos = (A - (2 * t * A) + (t * t * A)) + ((2 * t * B) - (2 * t * t * B)) + (t * t * C);
+        return besierPos;
+    }
+
+    public void RunBesier()
+    {
+        Vector3 besierStartPoint = this.transform.position;
+        A = besierStartPoint;
+        C = besierStartPoint + Camera.main.transform.forward * forwardDistance;
+        B = (A + C) / 2;
+        B.y += besierHeight;
+        isRunning = true;
+    }
+
+    public void LateUpdate()
+    {
+        if(!isRunning) return;
+        ///TODO: Check distance
+        transform.position = CalculateBesier(besierTime);
+    }
+
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private BesierCurve besierCurve;
+
+    private void Awake()
+    {
+        besierCurve = GetComponent<BesierCurve>();
+        besierCurve.enabled = true;
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            besierCurve.RunBesier();
+        }
+    }
+}
